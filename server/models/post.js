dffa41658d18fd5af26322631dac580ca41d1c8c
Leonard Atorough <leotorough@gmail.com>
2025-08-04 00:24:09 +0100

    update post model and service

--- server/models/post.js
+++ server/models/post.js
@@ -1,25 +1,30 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const postSchema = new mongoose.Schema({
-   title: { type: String, required: true },
-   slug: { type: String, required: true, lowercase: true },
-   published: { type: Boolean, default: false },
-   author: { type: String, required: true },
-   content: String,
-   tags: [String],
-   dateCreated: {
-      type: Date,
-      default: () => date.now(),
-      immutable: true
-   },
-   dateUpdated: Date,
-   comments: [
-      {
-         user: String,
-         content: String,
-         votes: Number
-      }
-   ]
+    title: { type: String, required: true },
+    slug: { type: String, required: true, lowercase: true },
+    published: { type: Boolean, default: false },
+    author: { type: String, required: true },
+    content: String,
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    dateCreated: {
+        type: Date,
+        default: () => date.now(),
+        immutable: true
+    },
+    dateUpdated: Date,
+    comments: [
+        {
+            user: String,
+            content: String,
+            votes: Number
+        }
+    ]
 });
 
 const Post = mongoose.model("Post", postSchema);
--- server/services/postService.js
+++ server/services/postService.js
@@ -3,10 +3,17 @@ var slugify = require("slugify");
 import { Post } from "../models/post.js";
 
 async function CreatePost(title, content, category, tags) {
+    const categoryId = "";
+    const tagIds = [];
+
     let post = new Post();
     post.title = title;
     post.slug = slugify(title);
     post.content = content;
+    post.category = categoryId;
+    post.tags = tagIds;
+
+    Post.create(post);
 }
 
 export { CreatePost };
